a0d99433b30a2ee2bae24e6f67a5738ff90d1344
Brennon <brennon246@gmail.com>
2013-02-06 22:26:04 -0500

    added activities for Registration, Change Password, and get username

--- Gshelf/gen/com/derpicons/gshelf/R.java
+++ Gshelf/gen/com/derpicons/gshelf/R.java
@@ -31,18 +31,27 @@ public final class R {
         public static final int trade=0x7f070008;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
-        public static final int activity_main_menu=0x7f030001;
+        public static final int activity_change_password=0x7f030000;
+        public static final int activity_get_user_name=0x7f030001;
+        public static final int activity_login=0x7f030002;
+        public static final int activity_main_menu=0x7f030003;
+        public static final int activity_register=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_login=0x7f060000;
-        public static final int activity_main_menu=0x7f060001;
+        public static final int activity_change_password=0x7f060000;
+        public static final int activity_get_user_name=0x7f060001;
+        public static final int activity_login=0x7f060002;
+        public static final int activity_main_menu=0x7f060003;
+        public static final int activity_register=0x7f060004;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int title_activity_change_password=0x7f040005;
+        public static final int title_activity_get_user_name=0x7f040006;
         public static final int title_activity_main_menu=0x7f040003;
+        public static final int title_activity_register=0x7f040004;
     }
     public static final class style {
         /** 
