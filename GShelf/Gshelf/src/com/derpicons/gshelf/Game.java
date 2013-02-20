9330c82c63de47844d1c91f59d1f82f9d365e82e
Brennon <brennon246@gmail.com>
2013-02-20 02:35:00 -0500

    Added Network and Game classes, incomplete

---
+++ GShelf/Gshelf/src/com/derpicons/gshelf/Game.java
@@ -0,0 +1,11 @@
+package com.example.g;
+
+public class Game {
+	
+	String title;
+	String platform;
+	String overview;
+	String genre;
+	String developer;
+
+}
---
+++ GShelf/Gshelf/src/com/derpicons/gshelf/Network.java
@@ -0,0 +1,51 @@
+package com.example.g;
+
+import java.util.ArrayList;
+
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.util.Log;
+
+
+public class Network {
+	
+
+	ArrayList<Game> getGames(String name)
+	{
+		//should use string.format
+		String url = "http://thegamesdb.net/api/GetGame.php?name=" + name;
+		
+		//create the http get from the url
+		HttpGet getMethod = new HttpGet(url);
+		
+		
+		
+		DefaultHttpClient client = new DefaultHttpClient();
+		
+		try {
+			ResponseHandler<String> responseHandler = new BasicResponseHandler();
+			String responseBody = client.execute(getMethod, responseHandler);
+			Log.i("RESPONSE BODY", responseBody);
+			
+			return parseResponse(responseBody);
+		} 
+		catch (Throwable t){
+			//can use t as a string to pass to logcat to display error
+			Log.i("ERROR", t.toString());
+			
+			return null;
+		}
+		
+		
+		
+	}
+	
+	ArrayList<Game> parseResponse(String response)
+	{
+		return null;
+	}
+
+}
