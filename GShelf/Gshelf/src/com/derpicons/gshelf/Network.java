2af1af94f8290086d8bcb180ef64c265655a990c
Brennon <brennon246@gmail.com>
2013-02-26 22:35:07 -0500

    Updated Network class to include XML parsing

--- GShelf/Gshelf/src/com/derpicons/gshelf/Network.java
+++ GShelf/Gshelf/src/com/derpicons/gshelf/Network.java
@@ -1,15 +1,28 @@
 package com.derpicons.gshelf;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 import android.util.Log;
+import android.util.Xml;
 
 
 public class Network {
@@ -27,11 +40,12 @@ public class Network {
 		
 		
 		DefaultHttpClient client = new DefaultHttpClient();
+		String responseBody = "nil";
 		
 		try {
 			ResponseHandler<String> responseHandler = new BasicResponseHandler();
-			String responseBody = client.execute(getMethod, responseHandler);
-			Log.i("RESPONSE BODY", responseBody);
+			 responseBody = client.execute(getMethod, responseHandler);
+			
 			
 			return parseResponse(responseBody);
 		} 
@@ -46,9 +60,115 @@ public class Network {
 		
 	}
 	
-	ArrayList<Game> parseResponse(String response)
+	//parses response from getGames API call
+	ArrayList<Game> parseResponse(String rawResponse) throws Exception
 	{
-		return null;
+		//converting String containg raw XML to inputstream
+		InputStream iStream = new ByteArrayInputStream(rawResponse.getBytes());
+		
+		//set up XML pull parser
+		XmlPullParser parser = Xml.newPullParser();
+		
+		parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+        parser.setInput(iStream, null);
+        parser.nextTag();
+        
+        parser.require(XmlPullParser.START_TAG, null, "Data");
+        
+        ArrayList<Game> games = new ArrayList<Game>();
+        
+        while (parser.next() != XmlPullParser.END_TAG){
+        	if(parser.getEventType() != XmlPullParser.START_TAG){
+        		continue;
+        	}
+        	
+        	String name = parser.getName();
+        	
+        	
+        	if(name.equals("Game")){
+        		games.add(parseGame(parser));
+        	}
+        	else{
+        		skip(parser);
+        	}
+        }
+        
+
+		iStream.close();
+		return games;
+	}
+	
+	Game parseGame(XmlPullParser parser) throws XmlPullParserException, IOException
+	{
+		
+		Game newGame = new Game();
+		while(parser.next() != XmlPullParser.END_TAG){
+			if(parser.getEventType() != XmlPullParser.START_TAG){
+				continue;
+			}
+			
+			String tagName = parser.getName();
+			
+			if(tagName.equals("GameTitle")){
+				newGame.title = parseGameTitle(parser,newGame);
+			
+			}
+			
+			else if(tagName.equals("Platform")){
+				newGame.platform = parsePlatform(parser, newGame);
+			}
+			
+			else if(tagName.equals("Overview")){
+				newGame.overview = parseOverview(parser, newGame);
+			}
+			
+			else{
+				skip(parser);
+			}
+			
+		
+			
+		}
+		
+		return newGame;
+	}
+	
+	String parseGameTitle(XmlPullParser parser, Game game) throws IOException, XmlPullParserException{
+		parser.require(XmlPullParser.START_TAG, null, "GameTitle");
+		String gameTitle = "null";
+		
+		if(parser.next() == XmlPullParser.TEXT){
+			gameTitle = parser.getText();
+			parser.nextTag();
+		}
+		
+		return gameTitle;
+	}
+	
+	String parsePlatform(XmlPullParser parser, Game game) throws IOException, XmlPullParserException{
+		parser.require(XmlPullParser.START_TAG, null, "Platform");
+		String platform = "null";
+		
+		if(parser.next() == XmlPullParser.TEXT){
+			platform = parser.getText();
+			parser.nextTag();
+		}
+		
+
+		
+		return platform;
+	}
+	
+	String parseOverview(XmlPullParser parser, Game game) throws IOException, XmlPullParserException{
+		parser.require(XmlPullParser.START_TAG, null, "Overview");
+		String overview = "null";
+		
+		if(parser.next() == XmlPullParser.TEXT){
+			overview = parser.getText();
+			parser.nextTag();
+		}
+		
+		return overview;
 	}
 	
 	String authenticate(String username, String password)
@@ -71,6 +191,23 @@ public class Network {
 		return isNetwork; 
 	}
 	
+	
+	private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+	    if (parser.getEventType() != XmlPullParser.START_TAG) {
+	        throw new IllegalStateException();
+	    }
+	    int depth = 1;
+	    while (depth != 0) {
+	        switch (parser.next()) {
+	        case XmlPullParser.END_TAG:
+	            depth--;
+	            break;
+	        case XmlPullParser.START_TAG:
+	            depth++;
+	            break;
+	        }
+	    }
+	 }
 	
 
 }
