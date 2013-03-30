6ddfa301b2206fd8517de4766925ea65112801b1
js09e <js09efsu@gmail.com>
2013-03-30 12:17:40 -0400

    Updated GameLibrary activity and added Games class

--- GShelf/Gshelf/src/com/derpicons/gshelf/GamesLibrary.java
+++ GShelf/Gshelf/src/com/derpicons/gshelf/GamesLibrary.java
@@ -1,8 +1,14 @@
 package com.derpicons.gshelf;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class GamesLibrary extends Activity {
 
@@ -10,6 +16,62 @@ public class GamesLibrary extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_games_library);
+
+		// Get list of games
+		
+		final ArrayList<Game> AGames = new ArrayList<Game>();
+		final Games LGames = new Games(AGames);
+		
+		// Display list of games
+
+		Button LGame = (Button) findViewById(R.id.LGame);
+		Button Search = (Button) findViewById(R.id.Search);
+
+		LGame.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				// Will bring user to the games page sending games info
+
+				int index = 1;
+
+				// Intent i = new Intent(getApplicationContext(),
+				// GameView.class);
+				Intent i = new Intent(getApplicationContext(), Login.class);
+				i.putExtra("key", LGames.getShowGames().get(index).getKey());
+				i.putExtra("title", LGames.getShowGames().get(index).getTitle());
+				i.putExtra("platform", LGames.getShowGames().get(index).getPlatform());
+				i.putExtra("overview", LGames.getShowGames().get(index).getOverview());
+				i.putExtra("genre", LGames.getShowGames().get(index).getGenre());
+				i.putExtra("developer", LGames.getShowGames().get(index).getDeveloper());
+				startActivity(i);
+			}
+		});
+
+		Search.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				String Type = "title";
+				final EditText SearchEditText = (EditText) findViewById(R.id.Search);
+
+				String LSearch = SearchEditText.getText().toString()
+						.toLowerCase();
+				Type.toLowerCase();
+
+				if (LSearch.length() != 0) {
+					Tag SearchTag = new Tag(LSearch, Type);
+					LGames.Search(SearchTag);
+				} else {
+					LGames.Refresh();
+				}
+			}
+		});
+
 	}
 
 	@Override
--- GShelf/Gshelf/src/com/derpicons/gshelf/Tag.java
+++ GShelf/Gshelf/src/com/derpicons/gshelf/Tag.java
@@ -6,19 +6,51 @@ public class Tag {
 	private String TagType;
 
 	public Tag(String tN, String tT) {
+		tT.toLowerCase();
+		tN.toLowerCase();
 		TagName = tN;
 		TagType = tT;
 	}
+
 	public Tag() {
 	}
-	
+
 	public boolean isMatch(Game x) {
 		if (TagType == "title") {
-
+			String gT = x.getTitle().toLowerCase();
+			if (gT.equals(TagName)) {
+				return true;
+			}
 		} else if (TagType == "platform") {
-
+			String gP = x.getPlatform().toLowerCase();
+			if (gP.equals(TagName)) {
+				return true;
+			}
 		} else {
+			String gD = x.getDeveloper().toLowerCase();
+			if (gD.equals(TagName)) {
+				return true;
+			}
+		}// TagType == developer
+		return false;
+	}
 
+	public boolean isPartialMatch(Game x) {
+		if (TagType == "title") {
+			String gT = x.getTitle().toLowerCase();
+			if (gT.contains(TagName)) {
+				return true;
+			}
+		} else if (TagType == "platform") {
+			String gP = x.getPlatform().toLowerCase();
+			if (gP.contains(TagName)) {
+				return true;
+			}
+		} else {
+			String gD = x.getDeveloper().toLowerCase();
+			if (gD.contains(TagName)) {
+				return true;
+			}
 		}// TagType == developer
 		return false;
 	}
