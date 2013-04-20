03dee170b81d02f05ba28440397311b2d0f6fbf1
Aaron Lathrop <ausnacht@gmail.com>
2013-04-20 01:31:35 -0400

    Action Bar: adding search bar
    Signed-off-by: Aaron Lathrop <ausnacht@gmail.com>

--- GShelf/Gshelf/src/com/derpicons/gshelf/Base_Activity.java
+++ GShelf/Gshelf/src/com/derpicons/gshelf/Base_Activity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class Base_Activity extends Activity 
 {
@@ -13,8 +14,10 @@ public class Base_Activity extends Activity
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) 
 	{
-		// Inflate the menu; this adds items to the action bar if it is present.
+		// Inflate the menu
 		getMenuInflater().inflate(R.menu.main_menu, menu);
+//		SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+//		return super.onCreateOptionMenu(menu);
 		return true;
 	}
 	
--- GShelf/Gshelf/src/com/derpicons/gshelf/GetUserName.java
+++ GShelf/Gshelf/src/com/derpicons/gshelf/GetUserName.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class GetUserName extends Base_Activity {
+public class GetUserName extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
