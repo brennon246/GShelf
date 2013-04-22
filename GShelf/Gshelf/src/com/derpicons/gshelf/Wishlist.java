d8b5655cc4b16a1a92c67f126ae8eff468b7661e
Aaron Lathrop <ausnacht@gmail.com>
2013-04-22 19:14:46 -0400

    Adding swipe to Wishlist
    Signed-off-by: Aaron Lathrop <ausnacht@gmail.com>

--- GShelf/Gshelf/src/com/derpicons/gshelf/Wishlist.java
+++ GShelf/Gshelf/src/com/derpicons/gshelf/Wishlist.java
@@ -1,7 +1,6 @@
 package com.derpicons.gshelf;
 
 import java.util.ArrayList;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +9,9 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.app.Activity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 public class Wishlist extends Base_Activity {
 
@@ -20,11 +22,21 @@ public class Wishlist extends Base_Activity {
 	int Userkey;
 	private SearchListAdapter SelectedSearchListAdapter;
 
+	// swipe constants
+	private static final int SWIPE_MIN_DISTANCE = 120;
+	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+	private GestureDetector gestureDetector;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_wishlist);
 		ctx = this;
+		
+		// Listen for swipes
+		gestureDetector = new GestureDetector(this,
+				new OnSwipeGestureListener());
 
 		Intent intent = getIntent();
 		Username = intent.getStringExtra("UserName");
@@ -82,4 +94,46 @@ public class Wishlist extends Base_Activity {
 
 	}
 
+	
+	// Swipe accessor function
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		return gestureDetector.onTouchEvent(event);
+	}
+
+	// Swipe Class
+	private class OnSwipeGestureListener extends
+			GestureDetector.SimpleOnGestureListener {
+		// Swipe movement evaluation
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+
+			float deltaX = e2.getX() - e1.getX();
+
+			if ((Math.abs(deltaX) < SWIPE_MIN_DISTANCE)
+					|| (Math.abs(velocityX) < SWIPE_THRESHOLD_VELOCITY)) {
+				return false; // insignificant swipe
+			} else {
+				if (deltaX < 0) { // left to right
+					handleSwipeLeftToRight();
+				} else { // right to left
+					handleSwipeRightToLeft();
+				}
+			}
+			return true;
+		}
+	}
+
+	// Handle swipe from left to right
+	private void handleSwipeLeftToRight() {
+		Intent i = new Intent(getApplicationContext(), GamesLibrary.class);
+		startActivity(i);
+	}
+
+	// Handle swipe from right to left
+	private void handleSwipeRightToLeft() {
+		Intent i = new Intent(getApplicationContext(), Marketplace.class);
+		startActivity(i);
+	}
 }
