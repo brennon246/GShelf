90e862717678bd8beae6097e5143e112f5a7439f
Aaron Lathrop <ausnacht@gmail.com>
2013-04-22 19:18:05 -0400

    Adding swipe to Marketplace
    Signed-off-by: Aaron Lathrop <ausnacht@gmail.com>

--- GShelf/Gshelf/src/com/derpicons/gshelf/Marketplace.java
+++ GShelf/Gshelf/src/com/derpicons/gshelf/Marketplace.java
@@ -1,7 +1,6 @@
 package com.derpicons.gshelf;
 
 import java.util.ArrayList;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +11,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.app.Activity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 public class Marketplace extends Base_Activity {
 
@@ -20,11 +22,22 @@ public class Marketplace extends Base_Activity {
 	String Username;
 	int Userkey;
 	ArrayList<Game> TradeGames;
+	
+	// swipe constants
+	private static final int SWIPE_MIN_DISTANCE = 120;
+	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+	private GestureDetector gestureDetector;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_marketplace);
+		
+		// Listen for swipes
+		gestureDetector = new GestureDetector(this,
+				new OnSwipeGestureListener());
 		
 		Button SearchButton = (Button) findViewById(R.id.buttonSearch);
 		Button YourTradesButton = (Button) findViewById(R.id.buttonYourTrades);
@@ -81,6 +94,48 @@ public class Marketplace extends Base_Activity {
 				startActivity(i);
 			}
 		});
+	}
+
+	// Swipe accessor function
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		return gestureDetector.onTouchEvent(event);
+	}
+
+	// Swipe Class
+	private class OnSwipeGestureListener extends
+			GestureDetector.SimpleOnGestureListener {
+		// Swipe movement evaluation
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+
+			float deltaX = e2.getX() - e1.getX();
+
+			if ((Math.abs(deltaX) < SWIPE_MIN_DISTANCE)
+					|| (Math.abs(velocityX) < SWIPE_THRESHOLD_VELOCITY)) {
+				return false; // insignificant swipe
+			} else {
+				if (deltaX < 0) { // left to right
+					handleSwipeLeftToRight();
+				} else { // right to left
+					handleSwipeRightToLeft();
+				}
+			}
+			return true;
+		}
+	}
+
+	// Handle swipe from left to right
+	private void handleSwipeLeftToRight() {
+		Intent i = new Intent(getApplicationContext(), Wishlist.class);
+		startActivity(i);
+	}
+
+	// Handle swipe from right to left
+	private void handleSwipeRightToLeft() {
+		Intent i = new Intent(getApplicationContext(), DealsView.class);
+		startActivity(i);
 	}
 
 }
