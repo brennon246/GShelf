22446e2dea405895c331dceee0d7103aabbdac8c
js09e <js09efsu@gmail.com>
2013-04-22 21:50:32 -0400

    updated trade class

--- GShelf/Gshelf/src/com/derpicons/gshelf/Trade.java
+++ GShelf/Gshelf/src/com/derpicons/gshelf/Trade.java
@@ -8,23 +8,8 @@ public class Trade {
 	private Boolean isLocal;
 	private Boolean isGlobal;
 	private String Location;
-	private Boolean isPaid;
-	private Boolean isShipped;
-	private Boolean isReceived;
+	private int Sellerid;
 	private int key;
-
-	public Trade(Game g, String p, Boolean isL, Boolean isG, String Loc,
-			Boolean isP, Boolean isS, Boolean isR, int k) {
-		tGame = g;
-		Price = p;
-		isLocal = isL;
-		isGlobal = isG;
-		Location = Loc;
-		isPaid = isP;
-		isShipped = isS;
-		isReceived = isR;
-		key = k;
-	}
 	
 	public Trade() {
 	}
@@ -59,30 +44,6 @@ public class Trade {
 
 	public void setLocation(String location) {
 		Location = location;
-	}
-
-	public Boolean getIsPaid() {
-		return isPaid;
-	}
-
-	public void setIsPaid(Boolean isPaid) {
-		this.isPaid = isPaid;
-	}
-
-	public Boolean getIsShipped() {
-		return isShipped;
-	}
-
-	public void setIsShipped(Boolean isShipped) {
-		this.isShipped = isShipped;
-	}
-
-	public Boolean getIsReceived() {
-		return isReceived;
-	}
-
-	public void setIsReceived(Boolean isReceived) {
-		this.isReceived = isReceived;
 	}
 
 	public int getKey() {
